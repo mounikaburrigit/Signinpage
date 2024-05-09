812a13450239e61519554c5e0690919b07f6a30f
mounikaburri <162602661+mounikaburri@users.noreply.github.com>
2024-05-09 11:53:09 +0530

    Add Project.js and project.css

---
+++ newapp/src/Project.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import "./project.css"
+
+
+const Project = () => {
+    return (
+        <div>
+        <div className='heading'>
+            <h1>Home</h1>
+            <h1>Flashcard</h1>
+            <h1>Contact</h1>
+            <h1>FAQ</h1>
+            <button className='btn'>Login</button>
+            
+            </div>
+            <div className='paragraph'>
+            <p>Flashcard></p> 
+            <p>Mathematics></p>
+            <p className='para'>Relation and Function</p>
+            </div>
+            <div>
+                <h1 className='head'>Relation and Functions (Mathematics)</h1>
+            </div>
+            
+            <div className='list1'>
+                <h1 className='studay'><u>study</u></h1> 
+                <h1>Quiz</h1>
+                <h1>Test</h1>
+                <h1>Game</h1>
+                <h1>Others</h1>
+                <div>
+                <div className='container'>
+                    <h1 className='heading1'>9+6+7x-2x-3</h1>
+                </div>
+                </div>
+                
+    
+            </div>
+            <div>
+                <h1 className='heading2'>create Flashcard</h1>
+            </div>
+            <div>
+                <h1>FAQ</h1>
+                <div>
+                    <p className='con'>Can education Flashcards can be used for all age groups</p>
+                    <p className='con'>how do education Flashcards work</p>
+                    <p className='con'>Can education be used for test preparation</p>
+                    </div>
+            </div>
+
+             </div>
+           
+
+            
+        
+    
+        
+    );
+};
+
+export default Project;
